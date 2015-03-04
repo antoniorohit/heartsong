a65ccc2e8800a1fe055d31ae33c7ddfc69207ac2
Antonio Rohit de Lima Fernandes <antoniorohit@gmail.com>
2015-03-03 16:35:34 -0800

    Pedantic mistake

--- HeartSong_pulseSensor_interrupt/Interrupt.ino
+++ HeartSong_pulseSensor_interrupt/Interrupt.ino
@@ -14,7 +14,6 @@ volatile int N = 0;
 
 void interruptSetup(){     
   // Initializes Timer2 to throw an interrupt every 2mS.
-  TCCR2A = 0x02;     // DISABLE PWM ON DIGITAL PINS 3 AND 11, AND GO INTO CTC (Clear Timer on Compare) MODE
   TCCR2B = 0x06;     // DON'T FORCE COMPARE, 256 PRESCALER 
   OCR2A = 0X7C;      // SET THE TOP OF THE COUNT TO 124 FOR 500Hz SAMPLE RATE
   TIMSK2 = 0x02;     // ENABLE INTERRUPT ON MATCH BETWEEN TIMER2 AND OCR2A
